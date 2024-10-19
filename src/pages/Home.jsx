825311a1c26ec1d9949e600e32c809a30b003e9e
jasmina-dev <astro.muon@gmail.com>
2024-10-19 12:16:31 -0400

    homepage

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import "../App.css";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+    </div>
+  );
+}
