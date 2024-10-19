3b22538cc32467415ea5cc2780be30d2c7e05eb7
Noah Ross <me@noah.dev>
2024-10-19 15:35:12 -0400

    better default

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000", // Use Vite's environment variable
+  baseURL: import.meta.env.VITE_API_URL || "http://127.0.0.1:5000", // Use Vite's environment variable
 });
 
 export default axiosInstance;
